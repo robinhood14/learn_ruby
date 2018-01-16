4b447bb92fd6d315bd3cb3a2a06e2bf8b2dc8938
robinhood14 <35225066+robinhood14@users.noreply.github.com>
2018-01-16 17:33:33 +0000

    Exercice OK

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,6 +1,6 @@
 def ftoc(num)
     return (num-32)*(5.0/9.0) #la fonction prend en entrée la température en F° et la convertie en C° suivant la formule mathématique. Par la suite, elle la retourne.
-end
+end 
 
 def ctof(num)
     return (num*9.0/5.0+32) #la fonction prend en entrée la température en C° et la convertie en F° suivant la formule mathématique. Par la suite, elle la retourne.
