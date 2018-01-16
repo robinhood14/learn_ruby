7fabf1770c9404414b399c6868c6dafb29054df6
robinhood14 <35225066+robinhood14@users.noreply.github.com>
2018-01-16 17:42:07 +0000

    Exercice OK

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -11,8 +11,15 @@ end
 # Defini la variable Array(input) pour la fonction Somme 
 def sum (arr)
     somme = 0 # initialiser la variable la somme à 0 #
-    arr.each do |x| # utilisation de l'itérateur each qui va prnedre chaque valeur de cette variable #
+    arr.each do |x| # utilisation de l'itérateur each qui va prendre chaque valeur de cette variable #
         somme += x # (somme = somme + x )chaque valeur de l'array est ajouter a la variable somme #
     end
-    return somme
+    return somme #
+end
+
+def multiply (arr)
+    multi = 0
+    arr.each do |x| #On fait la même chose pour la somme, sauf que pour cette fois au lieu d'additionner les éléments sur somme, on va les multiplier les éléments#         multi *= x
+    en
+    return multi 
 end
