64b139711c4b61f53ecc6b473cd62cfbaa52099c
robinhood14 <35225066+robinhood14@users.noreply.github.com>
2018-01-16 18:10:13 +0000

    exercice OK

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,8 +1,9 @@
+# On défini la fonction Hello qui va appeler la string "Hello!"
 def hello 
 	"Hello!"
 end 
 
-
+# on défini la fonction greet avec une variable who en entrée qui va appeler la string en entrée#
 def greet(who)
 	"Hello, #{who}!"
 end
