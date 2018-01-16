fdb0fdd251ff4be2bfd35432cdcb0181ee6c43b2
Robin Girard <robin.girard14@gmail.com>
2018-01-16 12:46:18 +0100

    Version Ok exercice 2 temparture

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,12 @@
-#write your code here
+# convert fahrenheit to celcius
+def ftoc (f_degree)
+    (f_degree -32.0) * 5.0/9.0  
+
+end 
+# convert celcius to fahrenheit
+def ctof (c_degree)
+    (c_degree * 9.0 / 5.0 ) + 32
+
+
+end 
+
